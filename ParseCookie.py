89663c58a869e49cd806e41675ba1f0d49eadc5a
ZhiyeTang <tongchiyip@gmail.com>
2023-07-15 23:13:52 +0800

    fourth commit

--- ParseCookie.py
+++ ParseCookie.py
@@ -4,9 +4,9 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-from config import URL
+from config import URL, CHROME_DRIVER_PATH
 
-service = Service(executable_path="/usr/local/bin/googledriver")
+service = Service(executable_path=CHROME_DRIVER_PATH)
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=options)
 driver.get(URL)
